d6ecfbd3c27708d72d2d04b760aac104b61990d7
idanr6-lab <idanr6@gmail.com>
2026-02-27 19:52:56 +0200

    Create game.js with game logic

---
+++ game.js
@@ -0,0 +1,18 @@
+// game.js - Game Logic
+
+// This is where your game logic will go.
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+    // Logic for playing a round of the game
+}
+
+function game() {
+    // Logic for playing the game
+}
+
+// Call the game function to start
+
+game();
